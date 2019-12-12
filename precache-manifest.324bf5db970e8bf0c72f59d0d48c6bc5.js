975adec2f28d759b1830432eb7d220bc656e50a0
Michael Dubois <ragefail1212@gmail.com>
2019-12-12 06:12:52 -0500

    Updates

---
+++ precache-manifest.324bf5db970e8bf0c72f59d0d48c6bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe586e0d18e6046573ffb89d83da324",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "da14fc93e4896cb1fb68",
+    "url": "/clicky-game/static/css/main.dc48cca2.chunk.css"
+  },
+  {
+    "revision": "eb68c72b8efdca53c8c8",
+    "url": "/clicky-game/static/js/2.214a3a19.chunk.js"
+  },
+  {
+    "revision": "da14fc93e4896cb1fb68",
+    "url": "/clicky-game/static/js/main.c91f700c.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
